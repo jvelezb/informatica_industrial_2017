e84d6afc06e2616d12e0c48085e9f967bc053d2b
Juan Velez Ballesteros <juanvelezballesteros@gmail.com>
2017-08-14 07:03:42 -0500

    first commit

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+variableNumero1 = 100 +21
+string1 = "hola"
+variable2 = 100 +1.1
+variable3 = 100/10.5
+print(variableNumero1)
+print(variableNumero1*4)
+print(type(variable2))
+print(variable3)##hola como estas
